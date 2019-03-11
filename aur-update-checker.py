30df63bef520395e67ec686836a42f4db89db4f8
Jason Reardon <reardon.jason@gmail.com>
2019-03-10 22:14:30 -0500

    updated output to match pacman verbage

--- aur-update-checker.py
+++ aur-update-checker.py
@@ -7,7 +7,5 @@ for package in aur_packages:
     local_version = aur.local_version(package)
     aur_version = aur.remote_version(package)
     if (local_version != aur_version):
-        print("{0} - local={1}  AUR={2}".format(package, local_version, aur_version))
-    else:
-        print("{0} - up to date".format(package))
+        print("{0} {1} -> {2}".format(package, local_version, aur_version))
 
